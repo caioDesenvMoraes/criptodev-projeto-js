9bdcd9d9c86153a4a5372eb4120e2b41edeaa0d6
Caio Moraes <caio.moraes.fbp@gmail.com>
2022-04-07 21:54:26 -0300

    Criação da Home

--- src/views/pages/Home.js
+++ src/views/pages/Home.js
@@ -1,10 +1,33 @@
-
+import Logo from "../../img/logo.jpg"
 
 let Home = {
     render: async () => {
         let view = `
-            <div>
-                <h1>Home</h1>
+            <div class="container">
+                <header class="header">
+                    <div class="wrapper-logo">
+                        <img src="${Logo}" alt="Logo"/>
+                    </div>
+                    <div class="social-media">
+                        <a href="https://www.instagram.com/caiomutley/" target="_blank">Instagram</a>
+                        <a href="https://www.linkedin.com/in/caio-moraes-8026a6158/" target="_blank">Linkedin</i></a>
+                        <a href="https://github.com/caioDesenvMoraes" target="_blank">Github</i></a>
+                    </div>
+                </header>
+                <main class="main">
+                    <div class="banner">
+                        <h1>Web Developer & Blockchain Developer</h1>
+                        <h3>Caio Moraes</h3>
+                        <a href="/">Formulário</a>
+                    </div>
+                    <div class="info">
+                        <h2>Sobre</h2>
+                        <p>Programador front-end e </p>
+                    </div>
+                </main>
+                <footer class="footer">
+                    <p>Copyright &copy; 2022 CM DEV - Todos os direitos reservados</p>
+                </footer>
             </div>
         `
 
